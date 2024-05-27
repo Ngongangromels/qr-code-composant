f5c7d788ff42a335080e8700d6e1117f87bc768c
Nkouakam Romels <romels064@gmail.com>
2024-05-27 13:00:23 +0200

    Challenge 0 qr-code componant avec recat

---
+++ src/component/QrCode.js
@@ -0,0 +1,22 @@
+import react from 'react';
+import image from '../images/image.png'
+import  './QrCode.css'
+
+
+function QrCode  ()  {
+     return(
+        <div className='qr-code-bloc'>
+            <img 
+               src= {image}
+               alt='qr-code-image'
+            />
+            <div>
+                <h1>Improve your front-end skills by building projects</h1>
+
+                <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+            </div>
+        </div>
+     )
+}
+
+export default QrCode
